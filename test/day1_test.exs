baa9076b0d193a9ae90a1c463d75a7b1a8d6d660
Max Carvajal <jcarvajal288@gmail.com>
2025-01-30 23:15:57 -0600

    Fix Day 1 full test

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -3,12 +3,12 @@ defmodule Day1Test do
   doctest Day1
 
   test "day 1 sample input" do
-    example_data = File.read!("lib/data/day1_example.txt")
-    assert Day1.reconcile_lists(example_data) == 11
+    data = File.read!("lib/data/day1_example.txt")
+    assert Day1.reconcile_lists(data) == 11
   end
 
   test "day 1 full input" do
-    example_data = File.read!("lib/data/day1_full.txt")
-    assert Day1.reconcile_lists(example_data) == 11
+    data = File.read!("lib/data/day1_full.txt")
+    assert Day1.reconcile_lists(data) == 2_769_675
   end
 end
